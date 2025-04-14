e0e0c16655633fd2fe8c58b0094f8c69cce6f2b6
Alexsander Albino <alexsander.santos.contato@gmail.com>
2025-04-14 16:32:29 -0300

    Feita questão 04

--- questao_03.py
+++ questao_03.py
@@ -3,4 +3,4 @@ while True:
     nome = input("Digite o nome do convidado: ")[:120]  # Limita a 120 caracteres
     if nome.lower() == "sair":  # Converte a entrada para minúsculas antes de comparar
         break
-    print("Seja muito bem-vindo ", nome)
+    print("Seja muito bem-vindo ", nome, "!")
---
+++ questao_04.py
@@ -0,0 +1,24 @@
+while True:
+    while True:  # Loop para validar a entrada
+        try:
+            numero = int(input("Insira um número inteiro: "))
+            break  # Sai do loop se a entrada for válida
+        except ValueError:
+            print("Entrada inválida! Por favor, insira um número inteiro.")
+
+    # Verifica se o número é primo
+    if numero > 1:
+        for i in range(2, int(numero ** 0.5) + 1):
+            if numero % i == 0:
+                print(f"{numero} não é um número primo.")
+                break
+        else:
+            print(f"{numero} é um número primo.")
+    else:
+        print(f"{numero} não é um número primo.")
+
+    # Pergunta ao usuário se deseja continuar
+    continuar = input("Deseja verificar outro número? (sim/não): ").strip().lower()
+    if continuar == "não" or continuar == "nao":
+        print("Encerrando o programa. Até mais!")
+        break
