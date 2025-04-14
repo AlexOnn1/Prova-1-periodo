7c11382cf3de0071a202d5e2bd8da0023685738f
Alexsander Albino <alexsander.santos.contato@gmail.com>
2025-04-14 16:46:52 -0300

    Feita Quastão 6

---
+++ Prova01/questão_06.py
@@ -0,0 +1,8 @@
+# Solicita a string ao usuário
+texto = input("Digite uma string: ")
+
+# Inverte o conteúdo da string
+texto_invertido = texto[::-1]
+
+# Exibe o resultado
+print("String invertida: ", texto_invertido)
