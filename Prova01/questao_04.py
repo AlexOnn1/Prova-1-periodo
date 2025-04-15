dd6750f32b188172444919c4a5002a4df1d871a1
Alexsander Albino dos Santos <160982032+AlexOnn1@users.noreply.github.com>
2025-04-15 10:37:04 -0300

    Correção questao_04.py

--- Prova01/questao_04.py
+++ Prova01/questao_04.py
@@ -7,7 +7,7 @@ while True:
             print("Entrada inválida! Por favor, insira um número inteiro.")
 
     # Verifica se o número é primo
-    if numero > 1:
+    if numero >= 1:
         for i in range(2, int(numero ** 0.5) + 1):
             if numero % i == 0:
                 print(numero, " não é um número primo.")
